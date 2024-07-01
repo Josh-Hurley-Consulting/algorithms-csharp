b5aacfa8358b438d3b99aba8cc1e41268eb97e55
Josh Hurley <53570882+joshongithub@users.noreply.github.com>
2024-07-01 16:20:27 -0400

    updated content

--- src/AlgorithmClassLibrary/Palindrome.cs
+++ src/AlgorithmClassLibrary/Palindrome.cs
@@ -15,8 +15,11 @@ namespace AlgorithmClassLibrary
         /// <returns>True/False if palindrome</returns>
         /// <remarks>
         /// Big O: 
-        /// * Time Complexity: O(log n) - Logarithmic, two characters are compared with each loop iteration
+        /// * Time Complexity: O(n) - Linear, pointers start n away from each other and get closer with each loop iteration
         /// * Space complexity: O(1) - Constant, memory requirements doesn't signficantly grow based on input
+        /// 
+        /// More about using two pointers and palindromes here:
+        /// https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/703/arraystrings/4501/
         /// </remarks>
         public static bool IsPalindrome(string word)
         {
